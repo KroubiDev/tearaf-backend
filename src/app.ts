4050ece7da92bba80262d4a66bb3c09414db890d
Amr Anwar <amranwar945@gmail.com>
2022-03-02 23:43:12 +0100

    fix: connect to db #16

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,8 @@ import {
 import { getUser, requestLogger, tokenParser } from './utils/middlewares';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import mongoose from 'mongoose';
+import { MONGODB_URI } from './utils/config';
 
 const app = express();
 app.use(cors());
@@ -24,6 +26,17 @@ app.post('/question', getUser, addQuestionController());
 
 const host = 'localhost';
 const port = '8080';
+
+mongoose
+	.connect(MONGODB_URI)
+	.then(() => {
+		return console.info(`[*] Successfully connected to db`);
+	})
+	.catch((error) => {
+		console.error('[!] Error connecting to database: ', error);
+		return process.exit(1);
+	});
+
 app.listen(8080, 'localhost', async () => {
-	console.info(`Server listing at http://${host}:${port}`);
+	console.info(`[*] Server listing at http://${host}:${port}`);
 });
